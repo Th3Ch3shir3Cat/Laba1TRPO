fb171f96dd98ff491c31dd5bf70d7fbf9b3bdcc2
gerbert <gerbert98@mail.ru>
2020-09-20 23:30:27 +0700

    Немного десктопа

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
-       // BinaryTree bt = new BinaryTree();
-       // ArrayList<Node> arrayTops = new ArrayList<>();
-
-       // for(int i = 0; i < 7; i++){
-       //     RandomString randomString = new RandomString();
-       //     bt.add(randomString.getResultString());
-        //}
-
-        //bt.traverseInOrder(bt.root, 0);
         TreeGUI treeGUI = new TreeGUI();
-        //System.out.println("\n" + bt.getNode_n(bt.root, 1).value);
-
-        /*bt.root = bt.getBalance(bt.arrayTops,0,6);
-        treeGUI = new TreeGUI(bt.root);
-*/
-
     }
 
 }
